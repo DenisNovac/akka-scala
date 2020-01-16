6ee7e79db094cd99f51ab2da2d77d916e62a9a4d
DenisNovac <ponydnk@gmail.com>
2020-01-16 22:43:07 +0500

    Ещё немного позанимался кодом, посмотрел логирование, но пока не прикрутил

--- http/medium-http-with-test/http-example/src/main/scala/com/learning/scala/SimpleHttp.scala
+++ http/medium-http-with-test/http-example/src/main/scala/com/learning/scala/SimpleHttp.scala
@@ -1,19 +1,24 @@
 package com.learning.scala
 
 import akka.actor.ActorSystem
-import akka.event.Logging
+import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
+
 
 object SimpleHttp extends App {
   implicit val system: ActorSystem = ActorSystem("simple-http")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
-  implicit val log = Logging(system, "main")
+  implicit val log: LoggingAdapter = Logging(system, "main")
   val port = 8080
+  val int = "localhost"
+
+  val route = HealthRoute.healthRoute
+
 
   val bindingFuture =
-    Http().bindAndHandle(HealthRoute.healthRoute, "localhost", port)
+    Http().bindAndHandle(route, int, port)
 
   log.info(s"Server started at the port $port")
 }
